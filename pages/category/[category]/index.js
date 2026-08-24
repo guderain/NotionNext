4bb88b76aca40842b803f756a47ff7f42df6cbed
wangguanxi <guanxi.wang@tujust.com>
2026-08-24 12:41:16 +0800

    还原接口转换格式

--- pages/category/[category]/index.js
+++ pages/category/[category]/index.js
@@ -57,11 +57,9 @@ export async function getStaticPaths() {
   const from = 'category-paths'
   const { categoryOptions } = await fetchGlobalAllData({ from })
   return {
-    paths: (Array.isArray(categoryOptions) ? categoryOptions : [])
-      .filter(category => category?.name)
-      .map(category => ({
-        params: { category: category.name }
-      })),
+    paths: Object.keys(categoryOptions).map(category => ({
+      params: { category: categoryOptions[category]?.name }
+    })),
     fallback: true
   }
 }
